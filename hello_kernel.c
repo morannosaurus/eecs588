8322668d1a2c0f8728d72ccb8ff0a580bd2cf9d3
Austin Yarger <ayarger@umich.edu>
2016-01-25 22:03:27 -0500

    Initial commit.

---
+++ hello_kernel.c
@@ -0,0 +1,15 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <sys/syscall.h>
+
+int init_module(void)
+{
+	register_symtable(NULL);	
+	printk(KERN_INFO "Hello world 1.\n");
+	return 0;
+}
+
+void cleanup_module(void)
+{
+	printk(KERN_INFO "Goodbye world 1.\n");
+}
